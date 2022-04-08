bec2de4139f2f0ad777557890b93f51456c2d10a
Semikin Nikita <semikin91@gmail.com>
2022-04-08 23:50:19 +0300

    Fixed px addition to mixins, upgrade typography

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <div class="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+    <h1 class="clr-primary text-nowrap">
+      NightVue
+    </h1>
     <img
       :src="require('@/assets/images/logo.png')"
       class="no-select mb-3">
-    <h1>
-      Кристально-чистый Vue 2 фреймворк
-    </h1>
+    <h3 class="text-center">Кристально-чистый Vue 2 фреймворк</h3>
     <p>
-      Создан чтобы создавать.
+      Создан, чтобы создавать.
     </p>
     </div>
   </div>
