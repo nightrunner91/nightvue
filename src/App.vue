f3f363b6b192c930f3c6235daf29538adf6dbec0
Nikita <semikin91@gmail.com>
2022-04-12 10:28:31 +0300

    Connected Onest font and started to make docs landing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
-    <div class="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center background position-relative">
+    <div
+      class="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center intro position-relative"
+      :class="windowScroll > 0 ? 'intro--scrolled' : ''">
       <img
         :src="require('@/assets/images/logo.png')"
         class="no-select mb-3">
-      <h1 class="clr-primary">
-        NightVue
+      <h1 class="clr-primary font-weight-400 mb-1">
+        <span class="clr-primary">Night</span>
+        <span class="clr-success">Vue</span>
       </h1>
-      <h3 class="text-center">Кристально-чистый Vue 2 фреймворк</h3>
+      <h3 class="text-center">Кристально-чистый Vue 2 фреймворк.</h3>
+    </div>
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col col-lg-12 p-3">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, voluptatem rerum nostrum accusantium similique porro fugiat? Ullam, dolorem voluptates ipsa dolor necessitatibus quisquam nesciunt reiciendis odio repellendus possimus rerum eligendi repellat dignissimos ea distinctio, est veniam, neque doloremque. Voluptate deleniti dolorum quod aspernatur maxime recusandae labore voluptatibus impedit, quam porro facere quisquam sapiente dicta, non fuga, modi cupiditate aut perspiciatis laborum repellat dolores rem facilis! Recusandae vel enim numquam porro dolores itaque sed aliquam odio, veritatis iusto. Dicta fugiat eligendi exercitationem soluta quidem a est nihil et iusto labore recusandae alias, ipsam corrupti nostrum quis odio magni repudiandae porro commodi eum expedita. Dolores, dolorum harum iure inventore expedita minus autem temporibus eaque culpa et! Fugiat, laudantium iure dolorem non impedit voluptate atque quis aut, itaque laboriosam, unde eveniet voluptatum fuga dolorum corporis? Odio assumenda soluta, accusantium eius velit culpa, amet consequatur ipsa repudiandae totam quas! Cum odit eligendi explicabo eaque excepturi! Libero temporibus eius maiores labore! At reprehenderit assumenda harum laborum, beatae corrupti? Ab excepturi ad eligendi enim ipsam quidem sapiente dolor repellendus provident debitis. Nobis molestias blanditiis sit quia facilis ut eaque officia modi. Natus doloremque quidem sint reiciendis aut ea alias ab in, quas aperiam tenetur necessitatibus ducimus!
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,7 +38,17 @@ export default {
     }
   },
   computed: {
+    windowScroll() {
+      return this.$store.state.config.windowScroll
+    },
 
+    windowSize() {
+      return this.$store.state.config.windowSize
+    },
+
+    innerHeight() {
+      return window.innerHeight
+    }
   },
   created() {
 
@@ -34,18 +56,29 @@ export default {
   mounted() {
     eventBus.$on('eventName', data => { this.methodName(data) })
 
-    this.watchWindowWidth()
-    window.addEventListener('resize', debounce(this.watchWindowWidth, 500))
+    this.watchWindowSize()
+    window.addEventListener('resize', debounce(this.watchWindowSize, 500))
+    window.addEventListener('scroll', debounce(this.watchWindowScroll, 50))
   },
   methods: {
     methodName(data) {
       return data
     },
 
-    watchWindowWidth() {
+    watchWindowSize() {
       const width = document.body.clientWidth
-      this.$store.commit('config/SAVE_WINDOW_WIDTH', width)
-    }
+      const height = document.body.clientHeight
+
+      this.$store.commit('config/SAVE_WINDOW_WIDTH', {
+        width: width,
+        height: height
+      })
+    },
+
+    watchWindowScroll() {
+      const scrollPos = window.scrollY
+      this.$store.commit('config/SAVE_WINDOW_SCROLL', scrollPos)
+    },
   },
 }
 </script>
@@ -53,29 +86,32 @@ export default {
 <style lang="scss">
   @import "styles/main";
 
-  .background {
+  .intro {
     &::before,
     &::after {
-      @extend .gradient-pro;
+      @include linear-gradient(map-get($gradients, pro));
+      @include transition(transform, lazy, emphasized);
+      @extend .pseudoelem;
+      @extend .z-minus-1;
 
-      content: '';
-      display: block;
-      position: fixed;
-      width: 20vw;
+      width: 30vw;
       height: 200vh;
-      z-index: -1;
+      top: -10%;
     }
 
     &::before {
-      top: 0;
-      left: 0;
+      left: -10%;
       transform: rotate(-25deg);
     }
 
     &::after {
-      top: 0;
-      right: 0;
+      right: -10%;
       transform: rotate(25deg);
+    }
+
+    &--scrolled {
+      &::before { transform: translateX(-100%); }
+      &::after { transform: translateX(100%); }
     }
   }
 </style>
--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -6,7 +6,11 @@ Vue.use(Vuex)
 export default {
   namespaced: true,
   state: {
-    windowWidth: undefined,
+    windowSize: {
+      width: undefined,
+      height: undefined
+    },
+    windowScroll: 0,
 
     // Dublicated in styles/core/_grid.scss
     breakpoints: {
@@ -18,9 +22,15 @@ export default {
     },
   },
   mutations: {
-    SAVE_WINDOW_WIDTH(state, data) {
-      state.windowWidth = data
-    }
+    SAVE_WINDOW_WIDTH(state, params) {
+      Object.keys(params).forEach(key => {
+        state.windowSize[key] = params[key]
+      })
+    },
+
+    SAVE_WINDOW_SCROLL(state, data) {
+      state.windowScroll = data
+    },
   },
   actions: {
 
