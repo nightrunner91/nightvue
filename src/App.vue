648c02159f41fbcacc5817dab8f7cc30969979a2
Nikita <semikin91@gmail.com>
2022-04-12 16:56:07 +0300

    Started colors component docs

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
   <div id="app">
     <div
       class="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center intro position-relative"
-      :class="windowScroll > windowSize.height / 5 ? 'intro--scrolled' : ''">
-      <!-- <div class="intro__backdrop w-100 h-50 z-minus-1 position-absolute" /> -->
+      :class="windowScroll > animationTriggerPosition ? 'intro--scrolled' : ''">
       <div class="intro__content d-flex flex-column justify-content-center align-items-center">
         <img
         :src="require('@/assets/images/logo.png')"
@@ -16,19 +15,35 @@
         <h3 class="text-center intro__text mt-0 font-weight-400">Кристально-чистый Vue 2 фреймворк.</h3>
       </div>
     </div>
-    <div>
+    <div
+      class="docs position-relative"
+      :class="windowScroll > animationTriggerPosition ? 'docs--appear' : ''"
+      :style="`top: -${animationTriggerPosition}px`">
       <div class="container">
         <div class="row">
           <div class="col cols-12 py-3">
-            <h1>1. Введение</h1>
+            <h1>Введение</h1>
             <h2>Чем является NightVue?</h2>
-            <p>NightVue — фреймворк для разработки на <a rel="nofollow" target="_blank" href="https://ru.vuejs.org/v2/guide/">Vue.js</a>. Главная его особенность — минимальное количество встроенных стилевых решений для распространённых компонентов (кнопок, форм, карточек, дропдаунов, модальных окон и т.д). Вместо этого NightVue предоставляет максимальную свободу разработки собственных веб-интерфейсов и множество полезных утилит для облегчения этого процесса.</p>
+            <p>NightVue — фреймворк для разработки на <a rel="nofollow" target="_blank" href="https://ru.vuejs.org/v2/guide/">Vue.js 2.x</a>. Его главная особенность — отсутствие интегрированных стилевых решений для распространённых компонентов (кнопок, форм, карточек, дропдаунов, модальных окон и т.д). Вместо этого NightVue предоставляет максимальную свободу разработки собственных веб-интерфейсов и множество полезных утилит для облегчения этого процесса. </p>
+
+            <p>В основу фреймворка легли наиболее полезные и удачные решения из <a rel="nofollow" target="_blank" href="https://getbootstrap.com/">Bootstrap 4.x</a>, многие из которых доработаны и улучшены. Разработчики, которые привыкли к Boostrap, легко вольются в предлагаемую нами среду разработки. Повсеместно используются рекомендации <a rel="nofollow" target="_blank" href="https://material.io/">Material Design</a>.  NightVue  работает на основе <a rel="nofollow" target="_blank" href="https://getbootstrap.com/">Vue CLI v.4</a> — одного из самых удобных инструментов для разработки на Vue.js.</p>
 
             <h2>Кому подойдёт NightVue?</h2>
-            <p>Front-end разработчикам, которым не нравится либо нет необходимости переписывать встроенные стилевые решения сторонних фреймворков и плагинов. Тем, кто предпочитает разрабатывать веб-интерфейсы “с чистого листа”. NightVue предоставляет гораздо б<span class="font-weight-500">о</span>льшую свободу в написании собственного кода, чем множество других Vue 2 фреймворков, в чём вы вскоре убедитесь самолично.</p>
+            <p>Front-end разработчикам, которым не нравится переписывать или удалять встроенные стилевые решения сторонних фреймворков и плагинов. Тем, кто предпочитает разрабатывать “с чистого листа”. NightVue предоставляет гораздо бо&#x301;льшую свободу в написании собственного кода, чем множество других  фреймворков. И в этом вы вскоре убедитесь.</p>
 
             <h2>Кому <span class="font-weight-700 clr-black">не</span> подойдёт NightVue?</h2>
-            <p>Front-end разработчикам, которым требуется база готовых стилевых решений для быстро старта разработки типизированных веб-интерфейсов. В таком случае нашей рекомендацией будет использование таких популярных решений как <a rel="nofollow" target="_blank" href="https://bootstrap-vue.org/">BootstrapVue</a>, <a rel="nofollow" target="_blank" href="https://vuetifyjs.com/en/">Vuetify</a>, <a rel="nofollow" target="_blank" href="https://www.creative-tim.com/vuematerial/">Vue Material</a> или любого другого на ваш вкус.</p>
+            <p>Front-end разработчикам, которым требуется база готовых стилевых решений для быстро старта разработки типизированных веб-интерфейсов. Либо тем, кто привык к такому подходу. В этом случае нашей рекомендацией будет использование таких популярных решений как <a rel="nofollow" target="_blank" href="https://bootstrap-vue.org/">BootstrapVue</a>, <a rel="nofollow" target="_blank" href="https://vuetifyjs.com/en/">Vuetify</a>, <a rel="nofollow" target="_blank" href="https://www.creative-tim.com/vuematerial/">Vue Material</a> или любого другого на ваш вкус.</p>
+
+            <h1>Документация</h1>
+            <ul class="list-unstyled">
+              <li
+                v-for="item, index in menu"
+                :key="`menu-${index}`">
+                <a :href="item.anchor">{{ item.title }}</a>
+              </li>
+            </ul>
+
+            <Colors />
           </div>
         </div>
       </div>
@@ -41,12 +56,23 @@
 import { eventBus } from './main'
 import debounce from 'lodash/debounce'
 
+import Colors from './views/docs/Colors.vue'
+
 export default {
-  components: {  },
+  components: { Colors },
   name: 'Home',
   data() {
     return {
-
+      menu: [
+        {
+          title: 'Установка NightVue',
+          anchor: '#installation'
+        },
+        {
+          title: 'Цвета',
+          anchor: '#colors'
+        },
+      ]
     }
   },
   computed: {
@@ -58,9 +84,9 @@ export default {
       return this.$store.state.config.windowSize
     },
 
-    innerHeight() {
-      return window.innerHeight
-    }
+    animationTriggerPosition() {
+      return this.windowSize.height / 4
+    },
   },
   created() {
 
@@ -78,8 +104,8 @@ export default {
     },
 
     watchWindowSize() {
-      const width = document.body.clientWidth
-      const height = document.body.clientHeight
+      const width = window.innerWidth
+      const height = window.innerHeight
 
       this.$store.commit('config/SAVE_WINDOW_WIDTH', {
         width: width,
@@ -99,13 +125,6 @@ export default {
   @import "styles/main";
 
   .intro {
-    &__backdrop {
-      @include linear-gradient(map-get($gradients, light));
-
-      top: 0;
-      left: 0;
-    }
-
     &__logo { @include transition(all, lazy, emphasized); }
     &__title { @include transition(all, lazy, decelerated, .15s); }
     &__text { @include transition(all, lazy, decelerated, .15s); }
@@ -137,7 +156,10 @@ export default {
       .intro {
         &__logo,
         &__title,
-        &__text { opacity: 0; }
+        &__text {
+          @include transition(all, short, accelerated);
+          opacity: 0;
+        }
 
         &__logo { transform: translateY(-160px); }
         &__title { transform: translateY(-50px); }
@@ -145,8 +167,16 @@ export default {
       }
 
       &::before { transform: translateX(-100%); }
-
       &::after { transform: translateX(100%); }
+    }
+  }
+
+  .docs {
+    @include transition(opacity, base, emphasized);
+    opacity: 0;
+
+    &--appear {
+      opacity: 1;
     }
   }
 </style>
---
+++ src/views/docs/Colors.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2 id="colors">Цвета</h2>
+    <p>По умолчанию в NightVue предустановлена библиотека из 15 стандартных цветов. Однако, вы можете полностью изменить её либо использовать свою.</p>
+    <ul class="d-flex list-unstyled colors">
+      <li
+        v-for="color in defaultColors"
+        :key="color"
+        class="color mr-75 radius-circle material-shadow-1" />
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Colors',
+    data() {
+      return {
+        defaultColors: 15
+      }
+    },
+
+  }
+</script>
+
+<style lang="scss">
+@import "../../styles/core/grid";
+@import "../../styles/core/colors";
+@import "../../styles/core/icon";
+
+.color {
+  @include icon-size(48);
+
+  @for $i from 1 through length($colors) {
+    &:nth-child(#{$i}) {
+      @include background(nth(nth($colors, $i), 1))
+    }
+  }
+}
+</style>
