e09607855d4358cc815b39ad68a92bb2c7a24c63
Nikita <semikin91@gmail.com>
2022-05-26 12:12:21 +0300

    docs and clean

--- README.md
+++ README.md
@@ -6,16 +6,9 @@
 
 ### What is NightVue?
 
-NightVue is a development framework for [Vue.js 2.x](https://ru.vuejs.org/v2/guide/). Its main feature is the absence of popular integrated components (buttons, forms, cards, dropdowns, modal windows, etc.) and the minimum number of preset styles. While many other frameworks offer ready-to-use UI solutions, NightVue gives you the maximum freedom to develop your own web interfaces. In fact, it is a set of useful tools for developers who like to do everything themselves.
+NightVue is a development framework for [Vue.js 2.x](https://ru.vuejs.org/v2/guide/). Its main feature is the absence of popular integrated components (buttons, forms, cards, dropdowns, modal windows, etc.) and the minimum number of preset styles. While many other frameworks offer ready-to-use UI solutions, NightVue gives you the maximum freedom to develop your own web interfaces. In fact, it is a set of useful tools for developers who like to do everything themselves. The ideological basis of the framework is the most useful utilities from [Bootstrap 4.x](https://getbootstrap.com/docs/4.6/getting-started/introduction/), many of which have been improved. NightVue uses best practices from [Material Design 2](https://material.io/) throughout. The framework is powered by [Vue CLI 4.x](https://cli.vuejs.org/), one of the most convenient tools for developing with Vue.js.
 
-The ideological basis of the framework is the most useful utilities from [Bootstrap 4.x](https://getbootstrap.com/docs/4.6/getting-started/introduction/)
-, many of which have been improved. NightVue uses best practices from [Material Design 2](https://material.io/) throughout. The framework is powered by [Vue CLI 4.x](https://cli.vuejs.org/), one of the most convenient tools for developing with Vue.js.
-
-### Who is it for?
-For Front-end developers who don't like rewriting the built-in styling solutions of third-party frameworks and plugins. Those who prefer to develop "from scratch". NightVue provides much more freedom to write your own code than many other similar frameworks, as you will soon see.
-
-### Who won't it suit?
-Front-end developers who need a base of ready-made UI solutions to quickly start developing typed web interfaces. Or those who are used to this approach. In this case, our recommendation would be to use popular solutions like [BootstrapVue](https://bootstrap-vue.org/), [Vuetify](https://vuetifyjs.com/en/), [Vue Material](https://www.creative-tim.com/vuematerial/), or whatever you like. In any case, we advise you to go over the detailed documentation prepared to get acquainted with the functionality of our framework.
+NightVue is build for Front-end developers who don't like rewriting the built-in styling solutions of third-party frameworks and plugins. Those who prefer to develop "from scratch". NightVue provides much more freedom to write your own code than many other similar frameworks, as you will soon see. For those who need framework with ready-to-use UI solutions for quick start, we would recomend to use popular solutions like [BootstrapVue](https://bootstrap-vue.org/), [Vuetify](https://vuetifyjs.com/en/), [Vue Material](https://www.creative-tim.com/vuematerial/), or whatever you like. In any case, we advise you to go over the detailed documentation prepared to get acquainted with the functionality of our framework.
 
 # Getting Started
 
@@ -38,7 +31,21 @@ npm run lint
 npm run lint --fix
 ```
 
+<<<<<<< Updated upstream
 # File Structure
+=======
+# What's included?
+
+* Vue.js v2.6.11
+* Vuex v3.4.0
+* Vue Router v2.6.11
+* Axios v0.24.0
+* eslint v6.7.2
+* svg-sprite-loader v6.0.11
+* lodash v4.17.21
+* vue2-touch-events v3.2.2
+* vue-waves-effect v1.0.0
+>>>>>>> Stashed changes
 
 # Documentation
 
@@ -240,7 +247,7 @@ gradientName: (
   list: ( String, String, String... )
 ),
 ```
-You can substitute in the direction parameter:
+You can pass in the direction parameter:
 
 * **Keyword**: to top, to top right, to right, to bottom right, to bottom, to bottom left, to left, to left top,
 * **Numeric value** in deg, rad, grad, or turn.
--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,19 @@
 <template>
   <div id="app">
-    <div
-      class="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center intro position-relative"
-      :class="windowScroll > animationTriggerPosition ? 'intro--scrolled' : ''">
-      <div
-        class="intro__decoration intro__decoration--left"
-        :style="decorationTransforms.left" />
-      <div
-        class="intro__decoration intro__decoration--right"
-        :style="decorationTransforms.right" />
-      <div
-        class="intro__content d-flex flex-column justify-content-center align-items-center"
-        :style="contentTransform">
-        <img
-        :src="require('@/assets/images/logo.png')"
-        class="no-select mb-3 intro__logo">
-        <h1 class="clr-primary mt-0 mb-1 intro__title">
-          <span class="clr-primary">Night</span>
-          <span class="clr-success">Vue</span>
-        </h1>
-        <h3 class="text-center intro__text mt-0 font-weight-400">Кристально-чистый Vue 2 фреймворк.</h3>
-      </div>
-    </div>
-    <!-- <nav class="menu position-fixed gradient-docsmenu z-plus-1000 w-25 h-100">
-
-    </nav> -->
-    <div
-      class="docs position-relative"
-      :class="windowScroll > animationTriggerPosition ? 'docs--appear' : ''"
-      :style="`top: -${animationTriggerPosition}px`">
-      <div class="container">
-        <div class="row">
-          <div class="col cols-12 py-3">
-            <Introduction />
-            <Installation />
-            <DocsMenu />
-            <Colors />
-            <Gradients />
-          </div>
-        </div>
-      </div>
-    </div>
+    <app-layout>
+      <router-view />
+    </app-layout>
   </div>
 </template>
 
 <script>
-// import AppLayout from "./layouts/AppLayout.vue"
+import AppLayout from "./layouts/AppLayout.vue"
 import { eventBus } from './main'
 import debounce from 'lodash/debounce'
 
-import Introduction from './views/docs/Introduction.vue'
-import Installation from './views/docs/Installation.vue'
-import DocsMenu from './views/docs/DocsMenu.vue'
-import Colors from './views/docs/Colors.vue'
-import Gradients from './views/docs/Gradients.vue'
-
 export default {
   components: {
-    Introduction,
-    Installation,
-    DocsMenu,
-    Colors,
-    Gradients,
+    AppLayout,
   },
   name: 'Home',
   data() {
@@ -76,37 +28,6 @@ export default {
 
     windowSize() {
       return this.$store.state.config.windowSize
-    },
-
-    animationTriggerPosition() {
-      return this.windowSize.height / 5
-    },
-
-    decorationTransforms() {
-      const styles = {
-        left: '',
-        right: ''
-      }
-
-      if (this.windowScroll < 1500) {
-        styles.left = `transform: rotate(-25deg) translateX(-${this.windowScroll}px)`
-        styles.right = `transform: rotate(25deg) translateX(${this.windowScroll}px)`
-
-        return styles
-      } else {
-        styles.left = 'transform: rotate(-25deg) translateX(-1500px)'
-        styles.right = 'transform: rotate(25deg) translateX(1500px)'
-
-        return styles
-      }
-    },
-
-    contentTransform() {
-      if (this.windowScroll < 750) {
-        return `transform: translateY(-${this.windowScroll / 2}px)`
-      } else {
-        return `transform: translateY(-750px)`
-      }
     },
   },
   created() {
@@ -144,72 +65,4 @@ export default {
 
 <style lang="scss">
   @import "styles/style";
-
-  .intro {
-    &__logo { @include transition(all, lazy, emphasized); }
-    &__title { @include transition(all, lazy, decelerated, .15s); }
-    &__text { @include transition(all, lazy, decelerated, .15s); }
-
-    &__decoration {
-      @include linear-gradient(map-get($gradients, 'primary-l3_primary'));
-      @include transition(transform, lazy, emphasized);
-      @extend .position-fixed;
-      @extend .z-minus-1;
-
-      width: 40vw;
-      height: 200vh;
-      top: -20%;
-
-      &--left {
-        left: -20%;
-      }
-
-      &--right {
-        right: -20%;
-      }
-    }
-
-    &__content {
-      @include transition(transform, lazy, decelerated)
-    }
-
-    &--scrolled {
-      .intro {
-        &__logo,
-        &__title,
-        &__text {
-          @include transition(all, short, accelerated);
-          opacity: 0;
-        }
-
-        &__logo { transform: translateY(-120px) scale(0.6); }
-        &__title { transform: translateY(-50px); }
-        &__text { transform: translateY(-40px); }
-      }
-    }
-  }
-
-  .docs {
-    @include transition(opacity, base, emphasized);
-    opacity: 0;
-
-    &--appear {
-      opacity: 1;
-    }
-  }
-
-  .menu {
-    @include transition(all, base, emphasized);
-
-    top: 0;
-    left: 0;
-    min-width: 320px;
-    max-width: 480px;
-    transform: translateX(calc(-100% + 10px));
-
-    &:hover {
-      @include shadow(1);
-      transform: translateX(0);
-    }
-  }
 </style>
