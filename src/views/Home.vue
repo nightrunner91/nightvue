75693a7cbf7af34d7d07549fdf704f993a02a24f
Nikita <semikin91@gmail.com>
2022-05-27 14:06:52 +0300

    Making new functions and mixins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-
+    <div class="selector">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, assumenda reiciendis quas est fugit iure dolor ipsa voluptatum officiis molestias?
+    </div>
   </div>
 </template>
 
