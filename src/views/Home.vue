2223e53a946318fcc3a51cff195546819006aca2
Semikin Nikita <semikin91@gmail.com>
2022-05-30 21:08:59 +0300

    Fixed wrong outputs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div>
     <div class="selector fill-secondary-dark-4 align-items-sm-start opacity-lg-40">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, assumenda reiciendis quas est fugit iure dolor ipsa voluptatum officiis molestias?
+      <div class="ololo icon-size-sm-24">
+        1
+      </div>
     </div>
   </div>
 </template>
