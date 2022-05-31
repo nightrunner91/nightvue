4d64ce225272d08611a369e468d046a66715ba96
Semikin Nikita <semikin91@gmail.com>
2022-05-31 22:45:23 +0300

    completing docs for mixins and function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="selector fill-secondary-dark-4 align-items-sm-start opacity-lg-40">
+    <div class="selector fill-secondary-dark-4 align-items-sm-start opacity-lg-40 gradient-primary_success">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, assumenda reiciendis quas est fugit iure dolor ipsa voluptatum officiis molestias?
       <div class="ololo icon-size-sm-24 icon-size-36">
         1
