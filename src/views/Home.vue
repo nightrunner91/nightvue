6b1332ef0f2584bde6751bd61936022729e8d11e
Semikin Nikita <semikin91@gmail.com>
2022-06-01 20:58:31 +0300

    Added docs for transitions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <div class="selector fill-secondary-dark-4 align-items-sm-start opacity-lg-40 gradient-primary_success">
+    <div class="selector">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, assumenda reiciendis quas est fugit iure dolor ipsa voluptatum officiis molestias?
-      <div class="ololo icon-size-sm-24 icon-size-36">
-        1
-      </div>
     </div>
   </div>
 </template>
