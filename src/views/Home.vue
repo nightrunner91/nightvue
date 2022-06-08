cb45219b8dc4eb66dbcb42b00887aa29743b152b
Nikita <semikin91@gmail.com>
2022-06-08 15:16:55 +0300

    Added rotation generator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-center minvh-100 minvw-100">
-    <div class="icon-size-48 bg-primary selector"></div>
+    <div class="icon-size-48 bg-primary selector rotate-minus-90 timing-ease speed-slow"></div>
   </div>
 </template>
 
