13692f5e20c07c59f18b5d47b0c842158a17682b
Nikita <semikin91@gmail.com>
2022-06-10 15:04:42 +0300

    Changed transitions from mixins to functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-center minvh-100 minvw-100">
-    <div class="icon-size-48 bg-primary selector rotate-minus-90 timing-ease speed-slow shadow-level-1"></div>
+    <div class="icon-size-48 bg-primary selector rotate-minus-90 radius-circle shadow-level-1"></div>
   </div>
 </template>
 
