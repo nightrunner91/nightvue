95eb4066b8afb9c1ef834f7ccf237e5e6259bc5c
Nikita <semikin91@gmail.com>
2022-06-16 10:57:10 +0300

    Working on grid mixins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="container">
-    <div class="row">
+  <div class="parent">
+    <div class="line">
       <div class="selector-1">1</div>
       <div class="selector-2">2</div>
     </div>
+    <div class="foo"></div>
   </div>
 </template>
 
