69e2756d85d67662021b4ae0de06cb3e03f679db
Nikita <semikin91@gmail.com>
2022-08-04 17:47:44 +0300

    Changed z-index gap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-1 d-flex flex-column justify-content-center align-items-center w-auto vw-100 vh-100">
+  <div class="bg-gray-10 d-flex flex-column justify-content-center align-items-center vw-100 vh-100">
     <img
       :src="require('@/assets/images/logo.png')"
       class="mb-75 no-select"
