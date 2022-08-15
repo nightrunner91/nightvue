c21b16ce91f117cec8e1109a15fb98454f309736
Nikita <semikin91@gmail.com>
2022-08-15 17:52:21 +0300

    Added docs component (beta)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="d-flex flex-column justify-content-center align-items-center vw-100 vh-100">
-    <img
-      :src="require('@/assets/images/logo.png')"
-      class="mb-75 no-select"
-      alt="logo">
-    <h1 class="mt-0">
-      <span class="clr-primary">Night</span>
-      <span class="clr-success">Vue</span>
-    </h1>
+  <div>
+    <div class="d-flex flex-column justify-content-center align-items-center vw-100 vh-100">
+      <img
+        :src="require('@/assets/images/logo.png')"
+        class="mb-75 no-select"
+        alt="logo">
+      <h1 class="mt-0">
+        <span class="clr-primary">Night</span>
+        <span class="clr-success">Vue</span>
+      </h1>
+    </div>
+    <docs />
   </div>
 </template>
 
 <script>
+import Docs from '../components/Docs'
+
 export default {
   name: 'Home',
+  components: { Docs },
 }
 </script>
